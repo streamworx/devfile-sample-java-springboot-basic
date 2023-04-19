11c8466503c2491690738e2095cfa0891c6e97b5
budi sentosa <budi@avasoft.co>
2023-04-19 09:11:38 +0700

    hello dev

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World!";
+        return "Hello World! build on dev";
     }
 
     public static void main(String[] args) {
